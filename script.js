90c6df45ca4a31504380aad0c1628fbc8c652a03
Prasiddha976 <om.prasiddha.gautam@gmail.com>
2026-09-17 20:58:39 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function sendMessage(event) {
-  event.preventDefault(); // Prevents page reload
-
   let name = document.getElementById("name").value.trim();
   let email = document.getElementById("email").value.trim();
   let msg = document.getElementById("message").value.trim();
 
-  if (name === "" || email === "" || msg === "") {
-    alert("Please fill in all fields!");
-  } else {
-    alert(`Thank you, ${name}! Your message has been sent.`);
-    document.querySelector(".contact-form").reset();
+  if (name !== "" && email !== "" && msg !== "") {
+    alert(`Thank you, ${name}! Your message has been sent to om.prasiddha.gautam@gmail.com.`);
   }
 }
